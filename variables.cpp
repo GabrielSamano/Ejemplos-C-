ab254aa1ec0d7be955166e604c1b52e2483c115a
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-29 13:26:39 -0500

    Creación de variables

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+
+// Creacion de variables
+
+int main() {
+    int MyNum = 15; // Creamos una variable con el nombre de MyNum que es de tio entero con valor de 15
+    MyNum = 10; // Ahora el valor de la variable es de 10
+    cout<< MyNum;
+    return 0;
+}
+
+// Lo que imprimira sera 10
