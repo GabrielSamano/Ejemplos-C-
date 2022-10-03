25f941abd7ec0e5b7d2774c79ca63d74159427a7
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-03 13:58:20 -0500

    Entrada de usuario

---
+++ User.cpp
@@ -0,0 +1,13 @@
+// Entrada de usuario en C++ con cin.
+
+int main() {
+    int x, y;
+    int sum;
+    cout<< "Ingresa un numero: ";
+    cin >> x;
+    cout<< "Ingresa otro numero: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Sum is: "<< sum;
+    return 0;
+}
