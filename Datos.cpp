3496ec021cc5c89693185dce790829d8716f7e0e
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-03 14:13:05 -0500

    Tipos de datos

---
+++ Datos.cpp
@@ -0,0 +1,10 @@
+// Tipos de datos en C++ 
+
+int main() {
+    int myNUm = 5;
+    float myFloat = 5.39;
+    double myDouble = 5.99;
+    char myLetter = 'c';
+    bool Booleano = true;
+    string myText= 'Hola';
+}
