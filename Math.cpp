8137bfac58c095734b5bb73c433c6d337706485f
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-05 14:18:47 -0500

    Matematicas en c++

---
+++ Math.cpp
@@ -0,0 +1,14 @@
+// Matematicas en c++
+
+int main() {
+    // Minimo
+    cout<< min(5,10);
+    //MAx
+    cout << max(5,10);
+    // Raiz cuadrada , redondear, Logaritmo natural
+    cout << sqrt(64);
+    cout << round(3.6);
+    cout << log(2);
+
+    return 0;
+}
