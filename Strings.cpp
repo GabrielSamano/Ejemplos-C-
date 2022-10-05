c3601eac70db74a1240f563ca80ca2da2d6bc3cc
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-05 14:10:49 -0500

    Strings

---
+++ Strings.cpp
@@ -0,0 +1,17 @@
+// Creando strings en c++
+
+int main () {
+    string greeting = "Hola";
+    cout << greeting;
+    // concatenar strings
+    string FirstName = "Julio";
+    string Lastname = "Cesar";
+    string FullName = FirstName + Lastname;
+    cout << FullName;
+
+    // Longitud del texto
+    string Txt = "ABCDEFGH"
+    cout << "La longitud del texto es: " << Txt.lenght();
+
+    return 0;
+}
