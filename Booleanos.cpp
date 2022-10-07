d4355908a075f6b7337680249bc5fe159bdbb1e7
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-07 08:42:56 -0500

    Ejemplos de booleanos

---
+++ Booleanos.cpp
@@ -0,0 +1,10 @@
+// Ejemplos de Booleanos en C++
+
+int main(){
+  bool EscribirEsdivertido = true;
+  bool EscribirEsFalso = false;
+  cout << EscribirEsdivertido << "\n";
+  cout << EscribirEsFalso;
+  
+  return 0;
+}
