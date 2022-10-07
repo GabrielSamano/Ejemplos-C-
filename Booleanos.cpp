292a76a7e76de11cb90d963e0b0c31437c5e4029
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-07 08:46:08 -0500

    Update Booleanos.cpp

--- Booleanos.cpp
+++ Booleanos.cpp
@@ -3,8 +3,11 @@
 int main(){
   bool EscribirEsdivertido = true;
   bool EscribirEsFalso = false;
+  int x = 10;
+  int y = 15;
   cout << EscribirEsdivertido << "\n";
   cout << EscribirEsFalso;
+  cout << (x < y);           //Imprimira true
   
   return 0;
 }
