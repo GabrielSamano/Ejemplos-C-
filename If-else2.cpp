2df3df92a3794e74b8adad0a86b7d7a67dabe39d
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-07 08:55:17 -0500

    Ejemplo If y else

---
+++ If-else2.cpp
@@ -0,0 +1,14 @@
+// Otro ejemplo de if y else 
+
+int main() {
+  int time = 20;
+  if (time < 20) {
+    cout << "Good Morning";
+  }else if(time < 10) {
+    cout << "Good Day";
+  }else{
+    cout << "Good Evening";
+  }
+  
+  return 0;
+}
