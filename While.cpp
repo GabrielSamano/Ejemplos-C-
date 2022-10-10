26a82893355c1516352ea69ab39c939dbcc09fae
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-10 09:36:48 -0500

    Bucle While

---
+++ While.cpp
@@ -0,0 +1,14 @@
+// Bucle con while
+
+int main() {
+  int i = 0;
+  while (i < 10) {
+    cout << i << "\n";
+    i++;
+  }
+  return 0;
+}
+
+// La salida sera 0,1,2,3,4,5,6,7,8,9 ya que especificamos que sea menor a 10 si declaramos que sea menor o igual a 10 entonces si se imprime el 10
+
+    
