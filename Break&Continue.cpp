8e57aca3e54f85211ed073a2f66a65339877f35a
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-10 10:21:22 -0500

    Break en bucle for

---
+++ Break&Continue.cpp
@@ -0,0 +1,12 @@
+// Detener o continuar en el bucle for
+
+int main () {
+  for ( int i = 0; i < 10; i++ ) {
+    if ( i == 4) {
+      break;
+    }
+  }
+  return 0;
+}
+
+// El resultado que imprimira sera 0,1,2,3 ya que declaramos que se detenga cuando i sea igual a 4 
