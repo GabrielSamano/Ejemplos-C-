88f6f7fb774d0773aa0b9975fa7f396c9ba5c4fe
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-10 10:32:54 -0500

    Continuar en bucle for

---
+++ Break&Continue2.cpp
@@ -0,0 +1,14 @@
+// Continuar en un bucle for 
+
+int main() {
+  for ( int i = 0; i < 10; i++) {
+    if ( i == 4 ) {
+      continue;
+    }
+    cout << i << "\n";
+  }
+  return 0;
+}
+
+// El resultado sera 0,1,2,3,5,6,7,8,9 
+      
