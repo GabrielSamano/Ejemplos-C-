1e5333c60dc8eb8dc7cb11016f3acde015468110
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-10 10:05:30 -0500

    Bucle for

---
+++ BucleFor.cpp
@@ -0,0 +1,10 @@
+// Creacion de un bucle for en c++
+
+int main() {
+  for ( int i = 0 ; i <= 10; i = i + 2) {
+    cout << i << "\n";
+  }
+  return 0;
+}
+
+// Imprimira los resultados de dos en dos 0,2,4,6,8,10 
