ca494c191853c870c434881dbf3d3d9ae5d23532
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-10 10:46:11 -0500

    Continue while

---
+++ B&Cwhile2.cpp
@@ -0,0 +1,15 @@
+// Continuar en bucle while 
+
+int main() {
+  int i = 0;
+  while ( i < 10 ) {
+    if ( i == 4 ) {
+      i++;
+      continue;
+    }
+    cout << i << "\n";
+  }
+  return 0;
+}
+
+    
