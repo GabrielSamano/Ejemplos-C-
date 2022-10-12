124920db3ee4fc071de50f3904be576e45ff6b89
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-12 09:35:56 -0500

    Arrays

---
+++ Arrays.cpp
@@ -0,0 +1,14 @@
+// Matrices declaradas en c++
+
+int main() {
+  string cars[5] = {"Volvo", "Ferrari", "BMW", "Dodge", "Nissan"};
+  
+  int MyNUms[3] = {11,15,20};
+  
+  cout << cars[1] << "\n";  // Impimira "Ferrari" 
+  cout << MyNUms[2];        // Imprimira 20
+  
+  return 0;
+}
+
+
