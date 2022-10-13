9188f63ec642c9ec550b7ca5987fa4350cb0d32b
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-13 10:03:20 -0500

    Function

---
+++ Function.cpp
@@ -0,0 +1,20 @@
+// Creando una funcion y llamandola tres veces
+ void MyFunction() {
+    cout << "Mi primera funcion.\n";
+ }
+
+int main() {
+  
+  MyFunction();
+  MyFunction();
+  MyFunction();
+  
+  return 0;
+}
+
+// Se vera de esta forma;
+// "Mi primera funcion."
+// "Mi primera funcion."
+// "Mi primera funcion."
+  
+  
