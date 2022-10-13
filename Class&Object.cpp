b8785fddc8c568e02f71f34ef74c45307ec4a919
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-13 10:29:35 -0500

    Clase y objeto

---
+++ Class&Object.cpp
@@ -0,0 +1,25 @@
+// Creando una clase y un objeto 
+
+class Myclass {
+  public;
+  int MyNum;
+  string MyString;
+};
+// Se creo una clase llamada Myclass con acceso publico y dos atributos de tipo entero y string
+
+int main() {
+  Myclass Myobj;
+  
+  Myobj.MyNum = 5;
+  Myobj.MyString = "Hello";
+  
+  cout << Myobj.MyNum << "\n";
+  cout << Myobj.MyString;
+  
+  return 0;
+  
+}
+
+
+    
+  
